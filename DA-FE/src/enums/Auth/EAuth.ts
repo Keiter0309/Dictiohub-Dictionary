9f97997deea3bd49e452976dedbab96a5e5ecc38
Keiter0309 <vuongtuankiet33@gmail.com>
2024-09-16 23:13:44 +0700

    Update Structure

---
+++ DA-FE/src/enums/Auth/EAuth.ts
@@ -0,0 +1,8 @@
+export enum Auth {
+    AUTH_REGISTER = "api/v1/auth/register",
+    AUTH_LOGIN= "api/v1/auth/login",
+    AUTH_FORGOT_PASSWORD = "api/v1/auth/forgot-password",
+    AUTH_RESET_PASSWORD = "api/v1/auth/reset-password",
+    AUTH_CHANGE_PASSWORD = "api/v1/auth/change-password",
+    AUTH_CLIENT_HOST = "http://localhost:9000",
+}
