fe0a01af943c4e602056fcf34e06d9bab1f31a36
Keiter0309 <vuongtuankiet33@gmail.com>
2024-12-02 00:36:16 +0700

    update: update aws sdk

--- DA-BE/src/services/aws/s3.service.ts
+++ DA-BE/src/services/aws/s3.service.ts
@@ -1,29 +1,33 @@
-import { S3 } from "aws-sdk";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import fs from "fs";
 import path from "path";
 
-const s3 = new S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
 export class S3Service {
+  private static client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    },
+  });
+
   // Upload audio files to S3
   public static async uploadAudioFiles() {
     const files = fs.readdirSync("audio");
     for (const file of files) {
       const filePath = path.join("audio", file);
       const fileContent = fs.readFileSync(filePath);
-      const params = {
-        Bucket: process.env.AWS_BUCKET_NAME || "",
+      
+      const command = new PutObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
         Key: `audio/${file}`,
         Body: fileContent,
         ContentType: "audio/mpeg",
-      };
+      });
 
       try {
-        const data = await s3.upload(params).promise();
-        console.log(`Uploaded ${file} successfully: ${data.Location}`);
+        const response = await this.client.send(command);
+        console.log(`Uploaded ${file} successfully`);
       } catch (err) {
         console.error(`Error uploading ${file}:`, err);
       }
